196d9bfcd37159ff66861b058c5c9c4dee43a267
Geoff Youngs <git@intersect-uk.co.uk>
2014-12-20 23:53:09 +0000

    Update test

--- test/pixbufutils_test.rb
+++ test/pixbufutils_test.rb
@@ -29,9 +29,15 @@ class TextPixbufUtils < MiniTest::Test
     assert_equal [78, 255, 78], pixels_after
   end
   def test_equalize
-    pb = Gdk::Pixbuf.new(fn = "test/IMG_8344.JPG")
+    FileUtils.mkdir_p("output/auto_equalize")
+    Dir.glob("samples/*.*").each do |jpeg|
+    pb = Gdk::Pixbuf.new(jpeg)
     equal = PixbufUtils.auto_equalize(pb)
-    equal.save("#{fn}.auto_equalize.jpg", "jpeg")
+    #equal.save("#{fn}.auto_equalize.jpg", "jpeg")
+
+    width = 600
+    pb = pb.scale(width, (width * pb.height/pb.width).to_i)
+    equal = equal.scale(pb.width, pb.height)
     img = Cairo::ImageSurface.new(:rgb24, pb.width, pb.height)
     cr = Cairo::Context.new(img)
     cr.set_source_pixbuf(pb)
@@ -42,10 +48,45 @@ class TextPixbufUtils < MiniTest::Test
     cr.line_to(pb.width, pb.height)
     cr.line_to(pb.width * 0.55, pb.height)
     cr.line_to(pb.width * 0.45, 0)
+    cr.save do
     cr.clip()
     cr.set_source_pixbuf(equal)
     cr.paint(1.0)
-    img.write_to_png("equalize.png")
+    end
+
+    cr.set_source_rgb(0,0,0)
+    cr.move_to(10, 10)
+
+    layout = cr.create_pango_layout
+	  layout.text = "Original"
+	  layout.font_description = Pango::FontDescription.new("Sans Bold 12")
+    cr.update_pango_layout(layout)
+    p layout.pixel_extents[0].width
+    cr.move_to(10,10)
+    cr.show_pango_layout(layout)
+    cr.set_source_rgb(1,1,1)
+    cr.move_to(8, 8)
+    cr.show_pango_layout(layout)
+    cr.set_source_rgb(0,0,0)
+    cr.move_to(10, 10)
+
+
+    layout = cr.create_pango_layout
+	  layout.text = "Corrected"
+	  layout.font_description = Pango::FontDescription.new("Sans Bold 12")
+    cr.update_pango_layout(layout)
+    width = layout.pixel_extents[0].width
+    cr.move_to(pb.width - 10 - width,10)
+    cr.show_pango_layout(layout)
+    cr.set_source_rgb(1,1,1)
+    cr.move_to(pb.width - width - 8, 8)
+    cr.show_pango_layout(layout)
+
+
+    img.write_to_png("output/auto_equalize/#{File.basename jpeg}.png")
+    pb.save("output/auto_equalize/#{File.basename jpeg, '.JPG'}.orig.jpg", "jpeg")
+    equal.save("output/auto_equalize/#{File.basename jpeg, '.JPG'}.equalized.jpg", "jpeg")
+    end
   end
 end
 
