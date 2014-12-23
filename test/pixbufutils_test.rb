86c0dd0cfa99ed3ce80599f334278e09d0418fbe
Geoff Youngs <git@intersect-uk.co.uk>
2014-12-23 10:48:44 +0000

    Update pixbufutils test to include montage output

--- test/pixbufutils_test.rb
+++ test/pixbufutils_test.rb
@@ -31,61 +31,64 @@ class TextPixbufUtils < MiniTest::Test
   def test_equalize
     FileUtils.mkdir_p("output/auto_equalize")
     Dir.glob("samples/*.*").each do |jpeg|
-    pb = Gdk::Pixbuf.new(jpeg)
-    equal = PixbufUtils.auto_equalize(pb)
-    #equal.save("#{fn}.auto_equalize.jpg", "jpeg")
+      pb = Gdk::Pixbuf.new(jpeg)
+      equal = PixbufUtils.auto_equalize(pb)
+      #equal.save("#{fn}.auto_equalize.jpg", "jpeg")
 
-    width = 600
-    pb = pb.scale(width, (width * pb.height/pb.width).to_i)
-    equal = equal.scale(pb.width, pb.height)
-    img = Cairo::ImageSurface.new(:rgb24, pb.width, pb.height)
-    cr = Cairo::Context.new(img)
-    cr.set_source_pixbuf(pb)
-    cr.paint(1.0)
+      width = 600
+      pb = pb.scale(width, (width * pb.height/pb.width).to_i)
+      equal = equal.scale(pb.width, pb.height)
+      img = Cairo::ImageSurface.new(:rgb24, pb.width, pb.height)
+      cr = Cairo::Context.new(img)
+      cr.set_source_pixbuf(pb)
+      cr.paint(1.0)
 
-    cr.move_to(pb.width * 0.45, 0)
-    cr.line_to(pb.width, 0)
-    cr.line_to(pb.width, pb.height)
-    cr.line_to(pb.width * 0.55, pb.height)
-    cr.line_to(pb.width * 0.45, 0)
-    cr.save do
-    cr.clip()
-    cr.set_source_pixbuf(equal)
-    cr.paint(1.0)
-    end
+      cr.move_to(pb.width * 0.45, 0)
+      cr.line_to(pb.width, 0)
+      cr.line_to(pb.width, pb.height)
+      cr.line_to(pb.width * 0.55, pb.height)
+      cr.line_to(pb.width * 0.45, 0)
+      cr.save do
+      cr.clip()
+      cr.set_source_pixbuf(equal)
+      cr.paint(1.0)
+      end
 
-    cr.set_source_rgb(0,0,0)
-    cr.move_to(10, 10)
+      cr.set_source_rgb(0,0,0)
+      cr.move_to(10, 10)
 
-    layout = cr.create_pango_layout
-	  layout.text = "Original"
-	  layout.font_description = Pango::FontDescription.new("Sans Bold 12")
-    cr.update_pango_layout(layout)
-    p layout.pixel_extents[0].width
-    cr.move_to(10,10)
-    cr.show_pango_layout(layout)
-    cr.set_source_rgb(1,1,1)
-    cr.move_to(8, 8)
-    cr.show_pango_layout(layout)
-    cr.set_source_rgb(0,0,0)
-    cr.move_to(10, 10)
-
-
-    layout = cr.create_pango_layout
-	  layout.text = "Corrected"
-	  layout.font_description = Pango::FontDescription.new("Sans Bold 12")
-    cr.update_pango_layout(layout)
-    width = layout.pixel_extents[0].width
-    cr.move_to(pb.width - 10 - width,10)
-    cr.show_pango_layout(layout)
-    cr.set_source_rgb(1,1,1)
-    cr.move_to(pb.width - width - 8, 8)
-    cr.show_pango_layout(layout)
+      layout = cr.create_pango_layout
+      layout.text = "Original"
+      layout.font_description = Pango::FontDescription.new("Sans Bold 12")
+      cr.update_pango_layout(layout)
+      cr.move_to(10,10)
+      cr.show_pango_layout(layout)
+      cr.set_source_rgb(1,1,1)
+      cr.move_to(8, 8)
+      cr.show_pango_layout(layout)
+      cr.set_source_rgb(0,0,0)
+      cr.move_to(10, 10)
 
 
-    img.write_to_png("output/auto_equalize/#{File.basename jpeg}.png")
-    pb.save("output/auto_equalize/#{File.basename jpeg, '.JPG'}.orig.jpg", "jpeg")
-    equal.save("output/auto_equalize/#{File.basename jpeg, '.JPG'}.equalized.jpg", "jpeg")
+      layout = cr.create_pango_layout
+      layout.text = "Corrected"
+      layout.font_description = Pango::FontDescription.new("Sans Bold 12")
+      cr.update_pango_layout(layout)
+      width = layout.pixel_extents[0].width
+      cr.move_to(pb.width - 10 - width,10)
+      cr.show_pango_layout(layout)
+      cr.set_source_rgb(1,1,1)
+      cr.move_to(pb.width - width - 8, 8)
+      cr.show_pango_layout(layout)
+
+
+      img.write_to_png(combine="output/auto_equalize/#{File.basename jpeg}.png")
+      pb.save(orig="output/auto_equalize/#{File.basename jpeg, '.JPG'}.orig.jpg", "jpeg")
+      equal.save(eql="output/auto_equalize/#{File.basename jpeg, '.JPG'}.equalized.jpg", "jpeg")
+      system("convert", orig, "histogram:#{ ho = orig.sub(/\.jpg$/, ".hist.jpg") }")
+      system("convert", eql, "histogram:#{ he= eql.sub(/\.jpg$/, ".hist.jpg") }")
+      system("montage", '-quality', '90', "-tile", "2x", "-mode", "Concatenate", orig, ho, eql, he, combine, "output/auto_equalize/#{File.basename jpeg}.montage.jpg" )
+      system("rm", "-f", ho, he, orig, eql)
     end
   end
 end
